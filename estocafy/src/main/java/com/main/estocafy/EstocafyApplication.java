7e0e31ff2b6782c28d951ef9e972d3df4ca2d2e6
João Lacerda <joaolacerda1862@gmail.com>
2025-05-29 12:28:20 -0300

    Initial commit estocafy-be

---
+++ estocafy/src/main/java/com/main/estocafy/EstocafyApplication.java
@@ -0,0 +1,13 @@
+package com.main.estocafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstocafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstocafyApplication.class, args);
+	}
+
+}
---
+++ estocafy/src/test/java/com/main/estocafy/EstocafyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.estocafy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstocafyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
